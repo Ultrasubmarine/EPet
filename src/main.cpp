8ed99400583d144a053fd8874c446c1a6921683d
Ultrasubmarine <sybmarina3000@yandex.ru>
2023-11-27 20:28:05 +0100

    Created xcode project;

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  EPet
+//
+//  Created by marina porkhunova on 27.11.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "---Hello, I'm your EPet <3---\n";
+    
+    
+    return 0;
+}
