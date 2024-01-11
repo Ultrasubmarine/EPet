a7d3a9abf6a6d2c0926017d7c8ca42780f7f20a3
Ultrasubmarine <sybmarina3000@yandex.ru>
2024-01-11 22:12:58 +0100

    Added non blocked input; (temp version)

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,32 @@
 #include <iostream>
 #include "Game.hpp"
 #include "PetInfo.hpp"
+#include <thread>
+#include <mutex>
 
+void Input()
+{
+    while(true)
+    {
+        char c;
+        std::cin>>c;
+        std::cout<<"print: "<<c<<std::endl;
+    }
+}
 
-int main(int argc, const char * argv[]) {
-  
-    Game myGame;    
+void Loop()
+{
+    Game myGame;
     myGame.Loop();
+}
+
+int main(int argc, const char * argv[])
+{
+    std::thread inputThread(&Input);
+    std::thread gameThread(&Loop);
     
+    inputThread.join();
+    gameThread.join();
+   
     return 0;
 }
