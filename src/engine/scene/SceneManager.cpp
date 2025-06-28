7134401e29bce8f228daa847421ff7290c944936
Ultrasubmarine <sybmarina3000@yandex.ru>
2025-06-28 21:39:15 +0200

    clean Scene Loading

---
+++ src/engine/scene/SceneManager.cpp
@@ -0,0 +1,122 @@
+//
+//  SceneManager.cpp
+//  EPet
+//
+//  Created by marina porkhunova on 28.02.2024.
+//
+
+#include <iostream>
+#include "SceneManager.hpp"
+
+#include "Scene.hpp"
+#include "Game.hpp"
+#include "ResourceManager.hpp"
+#include "SystemFactory.hpp"
+#include "Logging.hpp"
+
+#include "ISystem.hpp"
+
+#include "CommonComponents.hpp"
+
+#include "LoadComponents.hpp"
+#include "SaveComponents.hpp"
+
+#include "SceneUtils.hpp"
+
+using json = nlohmann::json;
+
+SceneManager::SceneManager(ResourceManager* r): _resourceManager(r)
+{
+    RegisterComponents();
+}
+
+/// TODO: think about another place
+void SceneManager::RegisterComponents()
+{
+    GenerateLoadingFunction<TestComponent>("TestComponent", &TestComponent::Load);
+    GenerateSaveFunction<TestComponent>("TestComponent", &TestComponent::Save);
+    
+    GenerateLoadingFunction<Sorting>("Sorting", &Sorting::Load);
+    GenerateSaveFunction<Sorting>("Sorting", &Sorting::Save);
+}
+
+SceneManager::~SceneManager()
+{
+    if(_currentScene)
+    {
+        DeleteScene();
+    }
+}
+
+void SceneManager::LoadScene(std::string id)
+{
+    if(!_resourceManager){
+        LOG_ERROR("SceneManager::LoadScene() resourceManager didn't find. Scene Loading canceled.");
+        return;
+    }
+    
+    auto data = _resourceManager->LoadScene(id);
+    if(!data) {
+        LOG_ERROR("SceneManager::LoadScene() scene \""<<id<<"\" didn't find. Scene Loading canceled.");
+        return;
+    }
+    
+    if(_currentScene)
+    {
+        DeleteScene();
+    }
+    
+    _currentScene = new Scene(id);
+    
+    //load systems
+    std::vector systemIds = GetSystemsIds(data);
+    LoadSystems(_currentScene, systemIds, _registry);
+    
+   //load objects
+    LoadObjects(data, _registry);
+
+    // end of loadding
+    _currentScene->Start();
+    OnCreate.Broadcast(_currentScene);
+    
+    delete data;
+    LOG_MESSAGE("SceneManager::LoadScene() scene \""<<id<<"\" was created.");
+};
+
+void SaveEntity();
+
+
+void SceneManager::SaveScene()
+{
+    json data;
+    data["scene"] = _currentScene->GetSceneId(); // TODO: don't really need this. save as name of file
+    
+    // save entities with components
+    for(const auto entity: _registry.view<entt::entity>())
+    {
+        json objectData;
+        SaveAllComponentInEntity(_registry, entity, objectData);
+        data["objects"].push_back(objectData);
+    }
+    
+    //TODO: save systems
+    
+    LOG_MESSAGE(data);
+    _resourceManager->SaveScene(_currentScene->GetSceneId(), &data);
+}
+
+void SceneManager::DeleteScene()
+{
+    _currentScene->Destroy();
+    OnDestroy.Broadcast(_currentScene);
+    delete _currentScene;
+}
+
+
+void SceneManager::Update(double dt)
+{
+    if(_currentScene)
+    {
+        _currentScene->Update(dt);
+    }
+}
---
+++ src/engine/scene/SceneUtils.hpp
@@ -0,0 +1,116 @@
+//
+//  SceneUtils.hpp
+//  EPet
+//
+//  Created by marina porkhunova on 28.06.2025.
+//
+
+#ifndef SceneUtils_hpp
+#define SceneUtils_hpp
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+#include "Logging.hpp"
+#include "Scene.hpp"
+
+#include "FactoryMethod.hpp"
+#include "SystemFactory.hpp"
+
+#include "LoadComponents.hpp"
+#include "SaveComponents.hpp"
+#include "registry.hpp"
+#include "json.hpp"
+
+using json = nlohmann::json;
+class Scene;
+
+//____________________________________
+// LOADING SCENE HELPING FUNCTIONS:
+std::vector<std::string> GetSystemsIds(const json* data)
+{
+    std::vector<std::string> systemIds{};
+    if ((*data).contains("systems")) {
+        auto systems =(*data)["systems"];
+        for (json::iterator it = systems.begin(); it != systems.end(); ++it) {
+
+            if(auto systemId = it.value().get<std::string>(); !systemId.empty())
+            {
+                systemIds.push_back(systemId);
+            }
+            else{
+                LOG_MESSAGE("LoadSystems() Empty system in json data");
+            }
+        }
+    }
+    return systemIds;
+}
+
+/// TODO: move to utilit.cpp
+void LoadSystems(Scene* scene, const std::vector<std::string>& systemIds, entt::registry& registry)
+{
+    for(const auto& systemId: systemIds)
+    {
+        ISystem* sys = SystemFactory::Instance().Create(systemId, registry);
+        if(!sys)
+        {
+            LOG_ERROR("LoadSystems() System with id \""<<systemId<<"\" didn't find in SystemFactory. system didn't added.");
+        }
+        scene->AddSystem(sys); // create system
+    }
+}
+
+entt::entity LoadObject(const json* data, entt::registry& registry)
+{
+    auto entity = registry.create();
+    
+    if((*data).contains("components"))
+    {
+        for (auto it_comp : (*data)["components"].items())
+        {
+            if(it_comp.value().contains("id"))
+            {
+                auto componentId = it_comp.value()["id"].get<std::string>();
+                LoadComponent(componentId.c_str(), registry, entity, it_comp.value());
+            }
+        }
+    }
+    // TODO: if child
+    // TODO: if prefab
+    
+    return entity;
+}
+
+#include "CommonComponents.hpp"
+
+void LoadObjects(const json* data, entt::registry& registry)
+{
+    if ((*data).contains("objects"))
+    {
+        auto entityes = (*data)["objects"];
+        for (json::iterator it = entityes.begin(); it != entityes.end(); ++it) {
+            
+            auto entity = LoadObject(&it.value(), registry);
+            
+// Test block code
+            LOG_MESSAGE(" load entity with: ");
+            if( auto comp = registry.try_get<TestComponent>(entity))
+            {
+                LOG_MESSAGE("   TestComponent: value: " << comp->value << " str:"<< comp->str);
+            }
+            if( auto comp = registry.try_get<Sorting>(entity))
+            {
+                LOG_MESSAGE("   Sorting: value: " << comp->layer);
+            }
+        }
+    }
+}
+
+// LOADING SCENE HELPING FUNCTIONS END
+//____________________________________
+
+// SAVING SCENE HELPING FUNCTIONS:
+// SAVING SCENE HELPING FUNCTIONS END
+
+#endif /* SceneUtils_hpp */
